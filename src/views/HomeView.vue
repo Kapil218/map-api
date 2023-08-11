99240c0a5ac5627e4dc562b6268bd3a873852dea
kapil singh rathore <kapilsinghrathore218@gmail.com>
2023-08-11 21:00:05 +0530

    done

---
+++ src/views/HomeView.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+    <google-places-autocomplete
+      id="autocomplete"
+      placeholder="Enter a location"
+      v-on:placechanged="getAddressData"
+    ></google-places-autocomplete>
+  </div>
+</template>
+
+<script>
+import GooglePlacesAutocomplete from "vue-google-autocomplete";
+
+export default {
+  components: {
+    GooglePlacesAutocomplete,
+  },
+  methods: {
+    getAddressData(place) {
+      console.log(place);
+    },
+  },
+};
+</script>
