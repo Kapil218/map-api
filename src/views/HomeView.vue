3df7c7db5d57ca2f038be377f30ee3a53ea263b3
kapil singh rathore <kapilsinghrathore218@gmail.com>
2023-08-11 23:51:03 +0530

    d

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,46 +1,107 @@
 <template>
   <div class="autocomplete-container">
-    <google-places-autocomplete
+    <input
       id="autocomplete"
+      type="text"
       placeholder="Enter a location"
       class="autocomplete-input"
-    ></google-places-autocomplete>
+      v-model="inputValue"
+      @input="onInput"
+    />
+    <ul class="chips-list">
+      <li
+        v-for="component in addressComponents"
+        :key="component"
+        class="chip"
+        @click="selectPlace(component)"
+      >
+        {{ component }}
+      </li>
+    </ul>
   </div>
 </template>
 
-<script>
-import GooglePlacesAutocomplete from "vue-google-autocomplete";
 
+<script>
 export default {
   data() {
-    return {};
+    return {
+      inputValue: "",
+      addressComponents: [],
+    };
   },
-  components: {
-    GooglePlacesAutocomplete,
+  methods: {
+    selectPlace(component) {
+      console.log(component);
+      this.inputValue = component;
+      this.addressComponents = [];
+    },
+    onInput() {
+      const autocompleteService =
+        new window.google.maps.places.AutocompleteService();
+      autocompleteService.getPlacePredictions(
+        { input: this.inputValue },
+        this.displayPredictions
+      );
+    },
+    displayPredictions(predictions, status) {
+      if (status === window.google.maps.places.PlacesServiceStatus.OK) {
+        // Display the predictions
+        console.log(predictions);
+        this.addressComponents = predictions.map(
+          (prediction) => prediction.description
+        );
+      }
+    },
+
+    initializeGoogleServices() {
+      // Now you can safely access the 'google' object
+      this.onInput();
+    },
+  },
+  mounted() {
+    // Load the Google Maps JavaScript API script here
+    const script = document.createElement("script");
+    script.src =
+      "https://maps.googleapis.com/maps/api/js?key=AIzaSyAldn0pDQzYAGR5KQ4oMXFjqSgByWVzWvk&libraries=places";
+    script.onload = () => {
+      this.initializeGoogleServices();
+    };
+    document.head.appendChild(script);
   },
 };
 </script>
-<style scoped>
+<style>
 .autocomplete-container {
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  height: 100vh;
+  position: relative;
+  width: 300px; /* Adjust as needed */
+  margin: 20px auto;
 }
 
 .autocomplete-input {
-  width: 600px;
+  width: 100%;
   padding: 10px;
   border: 1px solid #ccc;
   border-radius: 4px;
-  font-size: 14px;
-  box-shadow: 0 2px 4px rgba(0, 0, 0, 0.1);
-  transition: border-color 0.3s, box-shadow 0.3s;
 }
 
-.autocomplete-input:focus {
-  outline: none;
-  border-color: #66afe9;
-  box-shadow: 0 4px 6px rgba(102, 175, 233, 0.5);
+.chips-list {
+  list-style: none;
+  padding: 0 0;
+  margin: 0 0;
 }
+
+.chip {
+  display: inline-block;
+  background-color: #f0f0f0;
+  color: #333;
+  padding: 5px 10px;
+  margin: 5px;
+  border-radius: 4px;
+  font-size: 14px;
+}
+.chip:hover {
+  cursor: pointer;
+}
+/* Additional styling as needed */
 </style>
